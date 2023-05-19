cc4382dd1acc9ced01552d6cdb682eb233e2c655
Jean Paulino <jeanpaulino.ssp@gmail.com>
2023-05-19 17:59:24 -0300

    att

---
+++ ReduxMiddleware/middleware/localStorage.js
@@ -0,0 +1,12 @@
+const localStorage = (state) => (next) => (action) => {
+  const result = next(action);
+  if (action.localStorage !== undefined) {
+    window.localStorage.setItem(
+      action.localStorage,
+      JSON.stringify(action.payload)
+    );
+  }
+  return result;
+};
+
+export default localStorage;
